5243d5d9f9fea8d3ab685c38fd3ba809dccadae5
eunbeeko <minniemice98@gmail.com>
2021-02-13 00:23:40 +0900

    Add : Header with styled-component

--- src/components/Main/Header.js
+++ src/components/Main/Header.js
@@ -1,5 +1,29 @@
 import React from 'react'
+import styled from 'styled-components';
+import oc from 'open-color';
 
-export function Header () {
-    return(<div>아이콘 로그인 버트. </div>)
+const HeaderWrapper = styled.div`
+    /* 레이아웃 */
+    display: flex;
+    position: fixed;
+    align-items: center;
+    justify-content: center;
+    height: 60px;
+    width: 100%;
+    top: 0px;
+    z-index: 5;
+
+    /* 색상 */
+    background: ${oc.indigo[6]};
+    color: white;
+    border-bottom: 1px solid ${oc.indigo[7]};
+    box-shadow: 0 3px 6px rgba(0,0,0,0.10), 0 3px 6px rgba(0,0,0,0.20);
+
+    /* 폰트 */
+    font-size: 2.5rem;
+`;
+
+export const Header = () => {
+    return (<HeaderWrapper>
+        <span>Mint Camp</span></HeaderWrapper>)
 }
